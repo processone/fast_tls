e97b7bb6a6fbba789d7b5f0d253438637caef329
Christophe Romain <christophe.romain@process-one.net>
2015-02-04 13:39:27 +0100

    Update copyright up to 2015

--- src/p1_tls.erl
+++ src/p1_tls.erl
@@ -5,7 +5,7 @@
 %%% Created : 24 Jul 2004 by Alexey Shchepin <alexey@process-one.net>
 %%%
 %%%
-%%% p1_tls, Copyright (C) 2002-2013   ProcessOne
+%%% p1_tls, Copyright (C) 2002-2015   ProcessOne
 %%%
 %%% This program is free software; you can redistribute it and/or
 %%% modify it under the terms of the GNU General Public License as
