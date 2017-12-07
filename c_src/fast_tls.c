a2b2154d11280becbf3077e62f7b5621d52b54fd
Paweł Chmielowski <pchmielowski@process-one.net>
2017-12-07 10:10:02 +0100

    Libressl only offer pre 1.1 api even if it present version > 1.1

--- c_src/fast_tls.c
+++ c_src/fast_tls.c
@@ -57,7 +57,7 @@ typedef unsigned __int32 uint32_t;
 #define SSL_OP_NO_TICKET 0
 #endif
 
-#if OPENSSL_VERSION_NUMBER < 0x10100000L
+#if OPENSSL_VERSION_NUMBER < 0x10100000L || defined LIBRESSL_VERSION_NUMBER
 #define DH_set0_pqg(dh, dh_p, param, dh_g) (dh)->p = dh_p; (dh)->g = dh_g
 #define our_alloc enif_alloc
 #define our_realloc enif_realloc
@@ -347,7 +347,7 @@ static void unload(ErlNifEnv *env, void *priv) {
 
     for (i = 0; i < CRYPTO_num_locks(); i++)
         enif_mutex_destroy(mtx_buf[i]);
-#if OPENSSL_VERSION_NUMBER >= 0x10100000L
+#if OPENSSL_VERSION_NUMBER >= 0x10100000L && !defined LIBRESSL_VERSION_NUMBER
     OPENSSL_cleanup();
 #endif
 }
